cb582b0a5c1a074e3f697bab8bd978a9ea13f922
SerhanGyuler <serhan.gyuler@hotmail.com>
2025-05-13 11:19:52 +0200

    Add project files.

---
+++ Fjordingarnas_Bokningssystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fjordingarnas_Bokningssystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
