d1c340db86515652673a4d51ed57883cebcdcd97
glennka1337 <viktor.h@outlook.com>
2025-05-25 19:41:01 +0200

    Added API call to create a new customer.

--- Bookingsystem.API/Controllers/CustomerController.cs
+++ Bookingsystem.API/Controllers/CustomerController.cs
@@ -1,6 +1,34 @@
-﻿namespace BookingSystem.API.Controllers
+﻿using BookingSystem.API.Data;
+using Fjordingarnas_Bokningssystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.API.Controllers
 {
-    public class CustomerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(int id, string firstName, string lastName, string phoneNumber)
+        {
+            _context.Customers.Add(new Customer
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber
+            });
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
