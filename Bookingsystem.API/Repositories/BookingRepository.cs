369fd86a95e35e7633df070433893ee4b86c6e32
SerhanGyuler <serhan.gyuler@hotmail.com>
2025-05-28 17:04:21 +0200

    Finished Crud operations and added repository for booking

---
+++ Bookingsystem.API/Repositories/BookingRepository.cs
@@ -0,0 +1,49 @@
+﻿using BookingSystem.API.Data;
+using Fjordingarnas_Bokningssystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSystem.API.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BookingRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Booking>> GetAllAsync()
+        {
+            return await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Employee)
+                .Include(b => b.Services)
+                .ToListAsync();
+        }
+
+        public async Task<Booking?> GetByIdAsync(int id)
+        {
+            return await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Employee)
+                .Include(b => b.Services)
+                .FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task AddAsync(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+        }
+
+        public async Task DeleteAsync(Booking booking)
+        {
+            _context.Bookings.Remove(booking);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
