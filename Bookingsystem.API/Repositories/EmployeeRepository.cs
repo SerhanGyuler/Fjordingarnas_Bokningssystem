67b98bd120f915a3a82a35b7563811fb3ef484c6
Ozonlagret <tim.drejhede@gmail.com>
2025-06-02 22:42:17 +0200

    Added GetByIdWithServicesAsync to EmployeeRepository to retrieve an Employee with ID that includes services

--- Bookingsystem.API/Repositories/EmployeeRepository.cs
+++ Bookingsystem.API/Repositories/EmployeeRepository.cs
@@ -18,10 +18,17 @@ namespace BookingSystem.API.Repositories
             return await _context.Employees.FindAsync(id);
         }
 
+        public async Task<Employee?> GetByIdWithServicesAsync(int id)
+        {
+            return await _context.Employees
+                .Include(e => e.Services)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
         public async Task<IEnumerable<Employee>> GetAllAsync()
         {
             return await _context.Employees
-                .Include(b => b.Services)
+                .Include(e => e.Services)
                 .ToListAsync();
         }
     }
