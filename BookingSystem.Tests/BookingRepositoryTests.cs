6dd26c0b930cb29b1ca00cc737b74eb7e0d285bb
Ozonlagret <tim.drejhede@gmail.com>
2025-06-05 13:25:13 +0200

    Added BookingRepositoryTests along with a first test that verifies that GetAllAsync() returns data.

---
+++ BookingSystem.Tests/BookingRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Fjordingarnas_Bokningssystem.Models;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.API.Data;
+using BookingSystem.API.Repositories;
+
+namespace BookingSystem.Tests
+{
+    class BookingRepositoryTests
+    {
+        //1-6
+        [TestMethod]
+        public async Task GetAllAsync_ReturnsAllBookingsWithCustomerEmployeeAndServices()
+        {
+            var bookings = new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 1, 
+                    Customer = new Customer { Id = 1, FirstName = "Douglas", LastName = "MacArthur", PhoneNumber = "0523523535" },
+                    Employee = new Employee { Id = 1, FirstName = "Hamtaro", LastName = "Baker", PhoneNumber = "3535353555" },
+                    Services = new List<Service>
+                    {
+                        new Service { Id = 1, ServiceName = "Gräsklipp", Duration = TimeSpan.FromSeconds(1), Price = 1234}
+                    }
+                }
+            };
+
+            var mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(c => c.Bookings).ReturnsDbSet(bookings);
+
+            var repo = new BookingRepository(mockContext.Object);
+
+            var result = await repo.GetAllAsync();
+
+            var booking = result.Single();
+
+            Assert.Equals("Douglas", booking.Customer.FirstName);
+            Assert.Equals("Hamtaro", booking.Employee.FirstName);
+            Assert.Equals("Shave", booking.Services.Single().ServiceName);
+        }
+
+
+
+
+
+        //
+    }
+}
