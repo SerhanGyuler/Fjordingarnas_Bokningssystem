ece0ea8d1e85878b9ebf1b5213630186fbde9d39
SerhanGyuler <serhan.gyuler@hotmail.com>
2025-06-06 17:57:15 +0200

    Added GetAllCustomers to Service

--- Bookingsystem.API/Services/CustomerService.cs
+++ Bookingsystem.API/Services/CustomerService.cs
@@ -1,6 +1,29 @@
-﻿namespace BookingSystem.API.Services
+﻿using BookingSystem.API.Models.DTOs;
+using BookingSystem.API.Repositories;
+
+namespace BookingSystem.API.Services
 {
     public class CustomerService
     {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<List<CustomerDto>> GetAllCustomerDtosAsync()
+        {
+            var customers = await _customerRepository.GetAllCustomersAsync();
+
+            return customers.Select(c => new CustomerDto
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                PhoneNumber = c.PhoneNumber
+            }).ToList();
+        }
     }
+
 }
